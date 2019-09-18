a016cdd5904f7d6861ce6dd3208cf595037392a8
Ricardo Aguirre <ricardo.aguirre@mercadolibre.com>
2019-09-18 01:49:39 -0300

    actualizo la libreria local

--- vendor/github.com/ricardoAguirreSanchez/tp2-rna-hopfield/formulario/model.go
+++ vendor/github.com/ricardoAguirreSanchez/tp2-rna-hopfield/formulario/model.go
@@ -5,6 +5,13 @@ type Formulario struct {
 }
 
 type FormularioResultado struct {
+	FormularioBase1 Formulario
+	FormularioBase2 Formulario
+	FormularioBase3 Formulario
+	FormularioBase4 Formulario
+	FormularioBase5 Formulario
+	FormularioBase6 Formulario
+
 	FormularioInput  Formulario
 	FormularioOutput Formulario
 }
